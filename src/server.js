7bfd28c2c8eacf25bf825afd2ddd4a3a30b5b169
Udaydevika <ud.era2005@gmail.com>
2026-05-24 19:38:57 +0530

    final media upload fix

--- src/server.js
+++ src/server.js
@@ -95,10 +95,13 @@ filter: (req, res) => {
 })
 );
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({
+  limit: "100mb"
+}));
+
 app.use(express.urlencoded({
   extended: true,
-  limit: "50mb"
+  limit: "100mb"
 }));
 app.use(rateLimiter);
 
